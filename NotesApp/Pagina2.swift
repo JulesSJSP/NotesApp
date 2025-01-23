a6f6c611ff9e58f65c58f10ce72c86fbffadeca2
julesSJSP <jules.beausaert@jopie.be>
2025-01-23 12:09:18 +0100

    hsatck

---
+++ NotesApp/Pagina2.swift
@@ -0,0 +1,18 @@
+//
+//  Pagina2.swift
+//  NotesApp
+//
+//  Created by chapter 2 on 23/01/2025.
+//
+
+import SwiftUI
+
+struct Pagina2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Pagina2()
+}
