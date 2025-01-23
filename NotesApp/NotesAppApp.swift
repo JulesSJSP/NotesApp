8389b0f8f8de85c7dd01b54e00af491352a692d5
julesSJSP <jules.beausaert@jopie.be>
2025-01-23 11:22:49 +0100

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by chapter 2 on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
